02fa9558079bfb4a1a39e0ef22a82a04cf2ad0c9
Daniel Brito <dannyb829@gmail.com>
2020-02-24 00:35:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,15 +1,48 @@
-require_relative '../lib/display_board.rb'
-require_relative '../lib/input_to_index.rb'
-require_relative '../lib/valid_move?.rb'
-require_relative '../lib/position_taken.rb'
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+def position_taken?(board, index)
+  if board[index] == " "
+    return false
+  elsif board[index] == ""
+    return false
+  elsif board[index] == nil
+    return false
+  else board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index) == false
+    return true
+  end
+end
+
+def move(board, index, player)
+  board[index] = player
+end
 
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
-  index = user_input.to_i - 1
-  until valid_move?(board, index) == true
-    input_to_index(user_input)
-    valid_move?(board, index)
+  index = input_to_index(user_input)
+  if valid_move?(board, index) == true
+    move(board, index, player = "X")
+    display_board(board)
+  else
     puts "invalid move try again"
+    turn(board)
   end
 end
